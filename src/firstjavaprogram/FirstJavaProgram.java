a0fbcf3530c06b11526bb3960089bfd3139533e4
orati <orati@OraMmusi>
2024-08-11 18:26:23 +0200

    Third commit

---
+++ src/firstjavaprogram/FirstJavaProgram.java
@@ -0,0 +1,20 @@
+
+package firstjavaprogram;
+
+/**
+ *Student Name: Oratile Mmusi
+ * Student Number: ST10312691
+ * Assignment Name: Homework 1
+ * Submission due date: 28 July 2024
+ */
+public class FirstJavaProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
